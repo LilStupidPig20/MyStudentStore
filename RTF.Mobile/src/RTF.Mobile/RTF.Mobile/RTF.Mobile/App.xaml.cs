610d73c9c40fe087cbcae008c2ef5e5df0b8eb2f
Alexey Komissarov <aleksey.komissarov@saritasa.com>
2022-10-22 14:44:42 +0500

    Added RTF.Mobile application

---
+++ RTF.Mobile/src/RTF.Mobile/RTF.Mobile/RTF.Mobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using RTF.Mobile.Services;
+using RTF.Mobile.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RTF.Mobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
