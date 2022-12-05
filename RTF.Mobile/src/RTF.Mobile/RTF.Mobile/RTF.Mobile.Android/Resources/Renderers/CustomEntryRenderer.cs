2bdb34a77ec97d5e010ba70e5814eecc60f0f8c0
Alexey Komissarov <aleksey.komissarov@saritasa.com>
2022-12-05 15:02:04 +0500

    Added students image to login page

---
+++ RTF.Mobile/src/RTF.Mobile/RTF.Mobile/RTF.Mobile.Android/Resources/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms.Platform.Android;
+
+namespace RTF.Mobile.Droid.Resources.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+
+    }
+}
