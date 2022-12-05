206292247b297593399398d681d09f291f03d495
Alexey Komissarov <aleksey.komissarov@saritasa.com>
2022-12-05 22:11:17 +0500

    Added background for registration page

--- RTF.Mobile/src/RTF.Mobile/RTF.Mobile/RTF.Mobile.Android/Resources/Resource.designer.cs
+++ RTF.Mobile/src/RTF.Mobile/RTF.Mobile/RTF.Mobile.Android/Resources/Resource.designer.cs
@@ -16676,22 +16676,31 @@ namespace RTF.Mobile.Droid
 			public const int question = 2131165331;
 			
 			// aapt resource value: 0x7F070094
-			public const int shop = 2131165332;
+			public const int register_bg = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int students = 2131165333;
+			public const int register_new_bg = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int test_custom_background = 2131165334;
+			public const int shop = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int tooltip_frame_dark = 2131165335;
+			public const int students = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int tooltip_frame_light = 2131165336;
+			public const int test_custom_background = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int xamarin_logo = 2131165337;
+			public const int tooltip_frame_dark = 2131165337;
+			
+			// aapt resource value: 0x7F07009A
+			public const int tooltip_frame_light = 2131165338;
+			
+			// aapt resource value: 0x7F07009B
+			public const int wave = 2131165339;
+			
+			// aapt resource value: 0x7F07009C
+			public const int xamarin_logo = 2131165340;
 			
 			static Drawable()
 			{
